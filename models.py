1175232249320794c00c76cd63924b3045fdc3c0
coder <adhimansoor.k@gmail.com>
2026-07-22 21:58:11 +0530

    just starting my file seperation then debugging

---
+++ models.py
@@ -0,0 +1,22 @@
+
+from pydantic import BaseModel, field_validator
+class ensuring_everydata(BaseModel):
+        
+        job_name : str
+        company : str | None
+        salary : str | None
+        description : str | None
+        job_type : str | None
+        publication_date : str | None
+        candidate_location : str | None
+        url : str
+      
+        @field_validator("salary","company","description","job_type","publication_date","candidate_location")
+        @classmethod
+        def checking_data_None(cls,v):
+                      if v is None :
+                                return None
+                      if isinstance(v,str) and v.strip() == "":
+                              return None
+                      else :
+                              return v
